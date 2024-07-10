801dc500306c2c4583cd6fb99c7cf81d4f20d25d
Admin <timye720@outlook.com>
2024-07-10 13:47:13 +0800

    first commit

---
+++ CheckComments/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+
+namespace CheckComments
+{
+    public class CheckComments : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public ITaskItem[] Files { get; set; }
+
+        [Required]
+        public string SearchString { get; set; }
+
+        public override bool Execute()
+        {
+            foreach (var file in Files)
+            {
+                string content = File.ReadAllText(file.ItemSpec);
+                if (content.Contains(SearchString))
+                {
+                    Log.LogError($"The file '{file.ItemSpec}' contains '{SearchString}'. Build failed.");
+                    return false;//stop building
+                }
+            }
+            return true;// finish building
+        }
+    }
+}
